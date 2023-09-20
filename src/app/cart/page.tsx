2bdd7394cd9fcc192cbcd86970efd3285fc0ac4b
Wesley Ribas <wesleyribas2015@gmail.com>
2023-09-20 00:34:37 -0300

    Estrutura de um projeto NextJs

---
+++ src/app/cart/page.tsx
@@ -0,0 +1,17 @@
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Carrinho',
+  description: 'Veja todos os produtos que estão no seu carrinho',
+}
+export default function Cart() {
+  return (
+    <>
+
+      <main>
+        <h1>Carrinho</h1>
+      </main>
+
+    </>
+  )
+}
---
+++ src/app/product/page.tsx
@@ -0,0 +1,15 @@
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Nossos Produtos',
+  description: 'Conheça todos os nossos produtos',
+}
+export default function Products() {
+  return (
+    <>
+      <main>
+        <h1>Nossos Produtos</h1>
+      </main>
+    </>
+  )
+}
